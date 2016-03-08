dfc9c8c649dc826a01346c8b5a01556859e0f502
João Simões <nogravity00@outlook.com>
2016-03-08 17:20:01 +0000

    Implementation of SumAsync

--- SimplePersistence.UoW.EF/src/SimplePersistence.UoW.EF/EFAsyncQueryable-SumAsync.cs
+++ SimplePersistence.UoW.EF/src/SimplePersistence.UoW.EF/EFAsyncQueryable-SumAsync.cs
@@ -24,8 +24,6 @@
 namespace SimplePersistence.UoW.EF
 {
     using System;
-    using System.Collections;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Linq.Expressions;
     using System.Threading;
@@ -42,54 +40,224 @@ namespace SimplePersistence.UoW.EF
     /// <typeparam name="T">The entity type</typeparam>
     public partial class EFAsyncQueryable<T>
     {
+        /// <summary>
+        /// Asynchronously computes the sum of the sequence of values that is obtained by invoking a projection function on
+        ///                 each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the sum of the projected values..
+        /// </returns>
+#if NET40
         public Task<decimal> SumAsync(Expression<Func<T, decimal>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Sum(selector), ct);
         }
+#else
+        public async Task<decimal> SumAsync(Expression<Func<T, decimal>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.SumAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the sum of the sequence of values that is obtained by invoking a projection function on
+        ///                 each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the sum of the projected values..
+        /// </returns>
+#if NET40
         public Task<decimal?> SumAsync(Expression<Func<T, decimal?>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Sum(selector), ct);
         }
+#else
+        public async Task<decimal?> SumAsync(Expression<Func<T, decimal?>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.SumAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the sum of the sequence of values that is obtained by invoking a projection function on
+        ///                 each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the sum of the projected values..
+        /// </returns>
+#if NET40
         public Task<int> SumAsync(Expression<Func<T, int>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Sum(selector), ct);
         }
+#else
+        public async Task<int> SumAsync(Expression<Func<T, int>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.SumAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the sum of the sequence of values that is obtained by invoking a projection function on
+        ///                 each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the sum of the projected values..
+        /// </returns>
+#if NET40
         public Task<int?> SumAsync(Expression<Func<T, int?>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Sum(selector), ct);
         }
+#else
+        public async Task<int?> SumAsync(Expression<Func<T, int?>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.SumAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the sum of the sequence of values that is obtained by invoking a projection function on
+        ///                 each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the sum of the projected values..
+        /// </returns>
+#if NET40
         public Task<long> SumAsync(Expression<Func<T, long>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Sum(selector), ct);
         }
+#else
+        public async Task<long> SumAsync(Expression<Func<T, long>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.SumAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the sum of the sequence of values that is obtained by invoking a projection function on
+        ///                 each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the sum of the projected values..
+        /// </returns>
+#if NET40
         public Task<long?> SumAsync(Expression<Func<T, long?>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Sum(selector), ct);
         }
+#else
+        public async Task<long?> SumAsync(Expression<Func<T, long?>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.SumAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the sum of the sequence of values that is obtained by invoking a projection function on
+        ///                 each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the sum of the projected values..
+        /// </returns>
+#if NET40
         public Task<double> SumAsync(Expression<Func<T, double>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Sum(selector), ct);
         }
+#else
+        public async Task<double> SumAsync(Expression<Func<T, double>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.SumAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the sum of the sequence of values that is obtained by invoking a projection function on
+        ///                 each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the sum of the projected values..
+        /// </returns>
+#if NET40
         public Task<double?> SumAsync(Expression<Func<T, double?>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Sum(selector), ct);
         }
+#else
+        public async Task<double?> SumAsync(Expression<Func<T, double?>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.SumAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the sum of the sequence of values that is obtained by invoking a projection function on
+        ///                 each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the sum of the projected values..
+        /// </returns>
+#if NET40
         public Task<float> SumAsync(Expression<Func<T, float>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Sum(selector), ct);
         }
+#else
+        public async Task<float> SumAsync(Expression<Func<T, float>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.SumAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the sum of the sequence of values that is obtained by invoking a projection function on
+        ///                 each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the sum of the projected values..
+        /// </returns>
+#if NET40
         public Task<float?> SumAsync(Expression<Func<T, float?>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Sum(selector), ct);
         }
+#else
+        public async Task<float?> SumAsync(Expression<Func<T, float?>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.SumAsync(selector, ct);
+        }
+#endif
     }
 }
