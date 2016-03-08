dddf454150e0f0fc194e5f0e3ffab9332a8e1742
João Simões <nogravity00@outlook.com>
2016-03-08 17:28:13 +0000

    Implementation of AverageAsync

--- SimplePersistence.UoW.EF/src/SimplePersistence.UoW.EF/EFAsyncQueryable-AverageAsync.cs
+++ SimplePersistence.UoW.EF/src/SimplePersistence.UoW.EF/EFAsyncQueryable-AverageAsync.cs
@@ -24,8 +24,6 @@
 namespace SimplePersistence.UoW.EF
 {
     using System;
-    using System.Collections;
-    using System.Collections.Generic;
     using System.Linq;
     using System.Linq.Expressions;
     using System.Threading;
@@ -42,54 +40,224 @@ namespace SimplePersistence.UoW.EF
     /// <typeparam name="T">The entity type</typeparam>
     public partial class EFAsyncQueryable<T>
     {
+        /// <summary>
+        /// Asynchronously computes the average of a sequence of values that is obtained
+        ///                 by invoking a projection function on each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the average of the projected values.
+        /// </returns>
+#if NET40
         public Task<decimal> AverageAsync(Expression<Func<T, decimal>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Average(selector), ct);
         }
+#else
+        public async Task<decimal> AverageAsync(Expression<Func<T, decimal>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.AverageAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the average of a sequence of values that is obtained
+        ///                 by invoking a projection function on each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the average of the projected values.
+        /// </returns>
+#if NET40
         public Task<decimal?> AverageAsync(Expression<Func<T, decimal?>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Average(selector), ct);
         }
+#else
+        public async Task<decimal?> AverageAsync(Expression<Func<T, decimal?>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.AverageAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the average of a sequence of values that is obtained
+        ///                 by invoking a projection function on each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the average of the projected values.
+        /// </returns>
+#if NET40
         public Task<double> AverageAsync(Expression<Func<T, int>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Average(selector), ct);
         }
+#else
+        public async Task<double> AverageAsync(Expression<Func<T, int>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.AverageAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the average of a sequence of values that is obtained
+        ///                 by invoking a projection function on each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the average of the projected values.
+        /// </returns>
+#if NET40
         public Task<double?> AverageAsync(Expression<Func<T, int?>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Average(selector), ct);
         }
+#else
+        public async Task<double?> AverageAsync(Expression<Func<T, int?>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.AverageAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the average of a sequence of values that is obtained
+        ///                 by invoking a projection function on each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the average of the projected values.
+        /// </returns>
+#if NET40
         public Task<double> AverageAsync(Expression<Func<T, long>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Average(selector), ct);
         }
+#else
+        public async Task<double> AverageAsync(Expression<Func<T, long>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.AverageAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the average of a sequence of values that is obtained
+        ///                 by invoking a projection function on each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the average of the projected values.
+        /// </returns>
+#if NET40
         public Task<double?> AverageAsync(Expression<Func<T, long?>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Average(selector), ct);
         }
+#else
+        public async Task<double?> AverageAsync(Expression<Func<T, long?>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.AverageAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the average of a sequence of values that is obtained
+        ///                 by invoking a projection function on each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the average of the projected values.
+        /// </returns>
+#if NET40
         public Task<double> AverageAsync(Expression<Func<T, double>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Average(selector), ct);
         }
+#else
+        public async Task<double> AverageAsync(Expression<Func<T, double>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.AverageAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the average of a sequence of values that is obtained
+        ///                 by invoking a projection function on each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the average of the projected values.
+        /// </returns>
+#if NET40
         public Task<double?> AverageAsync(Expression<Func<T, double?>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Average(selector), ct);
         }
+#else
+        public async Task<double?> AverageAsync(Expression<Func<T, double?>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.AverageAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the average of a sequence of values that is obtained
+        ///                 by invoking a projection function on each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the average of the projected values.
+        /// </returns>
+#if NET40
         public Task<float> AverageAsync(Expression<Func<T, float>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Average(selector), ct);
         }
+#else
+        public async Task<float> AverageAsync(Expression<Func<T, float>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.AverageAsync(selector, ct);
+        }
+#endif
 
+        /// <summary>
+        /// Asynchronously computes the average of a sequence of values that is obtained
+        ///                 by invoking a projection function on each element of the input sequence.
+        /// </summary>
+        /// <param name="selector">A projection function to apply to each element. </param>
+        /// <param name="ct">A <see cref="T:System.Threading.CancellationToken"/> to observe while waiting for the task to complete.</param>
+        /// <returns>
+        /// A task that represents the asynchronous operation.
+        ///                 The task result contains the average of the projected values.
+        /// </returns>
+#if NET40
         public Task<float?> AverageAsync(Expression<Func<T, float?>> selector, CancellationToken ct = new CancellationToken())
         {
-            throw new NotImplementedException();
+            return Task.Factory.StartNew(() => _queryable.Average(selector), ct);
         }
+#else
+        public async Task<float?> AverageAsync(Expression<Func<T, float?>> selector, CancellationToken ct = new CancellationToken())
+        {
+            return await _queryable.AverageAsync(selector, ct);
+        }
+#endif
     }
 }
