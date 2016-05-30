b593ee8cbcc4b4d08467c7de4fbe3e687db606ca
João Simões <nogravity00@outlook.com>
2016-05-30 09:41:28 +0100

    Release 3.2.2

--- SimplePersistence.UoW.EF/src/SimplePersistence.UoW.EF/Properties/AssemblyInfo.cs
+++ SimplePersistence.UoW.EF/src/SimplePersistence.UoW.EF/Properties/AssemblyInfo.cs
@@ -40,6 +40,6 @@ using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("3.2.1")]
-[assembly: AssemblyInformationalVersion("3.2.1")]
+[assembly: AssemblyVersion("3.2.2")]
+[assembly: AssemblyInformationalVersion("3.2.2")]
 
